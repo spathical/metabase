2747dbc33a2466cfbb90c9b69491a35e45e9636c
Cam Saül <cammsaul@gmail.com>
2016-09-27 14:46:36 -0700

    Permissions checking for GET /api/card :lock:

--- src/metabase/models/card.clj
+++ src/metabase/models/card.clj
@@ -15,19 +15,11 @@
             [metabase.query-processor.resolve :as resolve]
             [metabase.util :as u]))
 
+
 (i/defentity Card :report_card)
 
 
-(defn- populate-query-fields [card]
-  (let [{query :query, database-id :database, query-type :type} (:dataset_query card)
-        table-id (or (:source_table query)  ; legacy (MBQL '95)
-                     (:source-table query))
-        defaults {:database_id database-id
-                  :table_id    table-id
-                  :query_type  (keyword query-type)}]
-    (if query-type
-      (merge defaults card)
-      card)))
+;;; ------------------------------------------------------------ Hydration ------------------------------------------------------------
 
 (defn dashboard-count
   "Return the number of Dashboards this Card is in."
@@ -42,6 +34,90 @@
   (if-let [label-ids (seq (db/select-field :label_id CardLabel, :card_id id))]
     (db/select Label, :id [:in label-ids], {:order-by [:%lower.name]})
     []))
+
+;;; ------------------------------------------------------------ Permissions Checking ------------------------------------------------------------
+
+(defn- permissions-path-set:mbql [{database-id :database, :as query}]
+  (let [{{:keys [source-table join-tables]} :query} (resolve/resolve (expand/expand query))]
+    (set (for [table (cons source-table join-tables)]
+           (perms/object-path database-id (:schema table) (:id table))))))
+
+(defn- permissions-path-set:native [read-or-write {database-id :database}]
+  #{((case read-or-write
+       :read  perms/native-read-path
+       :write perms/native-readwrite-path) database-id)})
+
+(defn- permissions-paths-set
+  "Return a set of required permissions object paths for CARD.
+   Optionally specify whether you want `:read` or `:write` permissions; default is `:read`.
+   (`:write` permissions only affects native queries)."
+  ([card]
+   (permissions-paths-set :read card))
+  ([read-or-write {{query-type :type, :as query} :dataset_query}]
+   (cond
+     (= query {})                     #{}
+     (= (keyword query-type) :native) (permissions-path-set:native read-or-write query)
+     (= (keyword query-type) :query)  (permissions-path-set:mbql query)
+     :else                            (throw (Exception. (str "Invalid query type: " query-type))))))
+
+
+(defn has-permissions?
+  "Does PERMISSIONS-SET grant access to all the objects referenced by CARD?
+   Optionally specify whether you want `:read` or `:write` permissions; default is `:read`."
+  ^Boolean
+  ([permissions-set card]
+   (has-permissions? :read permissions-set card))
+  ([read-or-write permissions-set card]
+   ;; we know that someone with root permissions can always access everything so skip the more complicated checking logic in that case
+   ;; TODO - it seems like we could also optimize a bit and check if full permissions for the Card's DB were present so we didn't have to expand/resolve in that case
+   (or (contains? permissions-set "/")
+       (perms/set-has-full-permissions-for-set? permissions-set (permissions-paths-set read-or-write card)))))
+
+(defn current-user-has-permissions?
+  "Does the current user have READ-OR-WRITE permissions for CARD?"
+  (^Boolean [read-or-write _ card-id]
+   ;; as above optimize away the check if we have root permissions
+   (or (contains? @*current-user-permissions-set* "/")
+       (current-user-has-permissions? read-or-write (Card card-id))))
+  (^Boolean [read-or-write card]
+   (has-permissions? read-or-write @*current-user-permissions-set* card)))
+
+
+;;; ------------------------------------------------------------ Dependencies ------------------------------------------------------------
+
+(defn card-dependencies
+  "Calculate any dependent objects for a given `Card`."
+  [this id {:keys [dataset_query]}]
+  (when (and dataset_query
+             (= :query (keyword (:type dataset_query))))
+    {:Metric  (q/extract-metric-ids (:query dataset_query))
+     :Segment (q/extract-segment-ids (:query dataset_query))}))
+
+
+;;; ------------------------------------------------------------ Revisions ------------------------------------------------------------
+
+(defn serialize-instance
+  "Serialize a `Card` for use in a `Revision`."
+  [_ _ instance]
+  (->> (dissoc instance :created_at :updated_at)
+       (into {})                                 ; if it's a record type like CardInstance we need to convert it to a regular map or filter-vals won't work
+       (m/filter-vals (complement delay?))))
+
+
+
+;;; ------------------------------------------------------------ Lifecycle ------------------------------------------------------------
+
+
+(defn- populate-query-fields [card]
+  (let [{query :query, database-id :database, query-type :type} (:dataset_query card)
+        table-id (or (:source_table query) ; legacy (MBQL '95)
+                     (:source-table query))
+        defaults {:database_id database-id
+                  :table_id    table-id
+                  :query_type  (keyword query-type)}]
+    (if query-type
+      (merge defaults card)
+      card)))
 
 
 (defn- pre-insert [{:keys [dataset_query], :as card}]
@@ -62,29 +138,6 @@
   (db/cascade-delete! 'CardLabel :card_id id))
 
 
-;;; ## ---------------------------------------- REVISIONS ----------------------------------------
-
-
-(defn serialize-instance
-  "Serialize a `Card` for use in a `Revision`."
-  [_ _ instance]
-  (->> (dissoc instance :created_at :updated_at)
-       (into {})                                 ; if it's a record type like CardInstance we need to convert it to a regular map or filter-vals won't work
-       (m/filter-vals (complement delay?))))     ; TODO - I don't think this is necessary anymore !
-
-
-;;; ## ---------------------------------------- DEPENDENCIES ----------------------------------------
-
-(defn card-dependencies
-  "Calculate any dependent objects for a given `Card`."
-  [this id {:keys [dataset_query]}]
-  (when (and dataset_query
-             (= :query (keyword (:type dataset_query))))
-    {:Metric  (q/extract-metric-ids (:query dataset_query))
-     :Segment (q/extract-segment-ids (:query dataset_query))}))
-
-(declare current-user-has-permissions?)
-
 (u/strict-extend (class Card)
   i/IEntity
   (merge i/IEntityDefaults
@@ -103,35 +156,3 @@
 
   dependency/IDependent
   {:dependencies card-dependencies})
-
-
-;;; ------------------------------------------------------------ Permissions Checking ------------------------------------------------------------
-
-(defn- permissions-paths-set
-  "Return a set of required permissions object paths for CARD.
-   Optionally specify whether you want `:read` or `:write` permissions; default is `:read`.
-   (`:write` permissions only affects native queries)."
-  ([card]
-   (permissions-paths-set :read card))
-  ([read-or-write {{database-id :database, query-type :type, :as query} :dataset_query}]
-   (if (= (keyword query-type) :native)
-     #{((case read-or-write
-          :read  perms/native-read-path
-          :write perms/native-readwrite-path) database-id)}
-     (let [{{:keys [source-table join-tables]} :query} (resolve/resolve (expand/expand query))]
-       (set (for [table (cons source-table join-tables)]
-              (perms/object-path database-id (:schema table) (:id table))))))))
-
-(defn has-permissions?
-  "Does PERMISSIONS-SET grant access to all the objects referenced by CARD?
-   Optionally specify whether you want `:read` or `:write` permissions; default is `:read`."
-  ^Boolean
-  ([permissions-set card]
-   (has-permissions? :read permissions-set card))
-  ([read-or-write permissions-set card]
-   (perms/set-has-full-permissions-for-set? permissions-set (permissions-paths-set read-or-write card))))
-
-(defn current-user-has-permissions?
-  "Does the current user have READ-OR-WRITE permissions for CARD?"
-  ^Boolean [read-or-write card]
-  (has-permissions? read-or-write @*current-user-permissions-set* card))
