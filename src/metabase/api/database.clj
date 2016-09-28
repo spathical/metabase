c3387cae3bb9c801ba04fb13873669d2c7241247
Cam Saül <cammsaul@gmail.com>
2016-09-28 15:49:36 -0700

    Reorganize metabase.api.database :shower:

--- src/metabase/api/database.clj
+++ src/metabase/api/database.clj
@@ -22,33 +22,8 @@
   [symb value :nillable]
   (checkp-with driver/is-engine? symb value))
 
-(defn test-database-connection
-  "Try out the connection details for a database and useful error message if connection fails, returns `nil` if connection succeeds."
-  [engine {:keys [host port] :as details}]
-  (when-not config/is-test?
-    (let [engine           (keyword engine)
-          details          (assoc details :engine engine)
-          response-invalid (fn [field m] {:valid false
-                                          field m        ; work with the new {:field error-message} format
-                                          :message m})]  ; but be backwards-compatible with the UI as it exists right now
-      (try
-        (cond
-          (driver/can-connect-with-details? engine details :rethrow-exceptions) nil
-          (and host port (u/host-port-up? host port))                           (response-invalid :dbname (format "Connection to '%s:%d' successful, but could not connect to DB." host port))
-          (and host (u/host-up? host))                                          (response-invalid :port   (format "Connection to '%s' successful, but port %d is invalid." port))
-          host                                                                  (response-invalid :host   (format "'%s' is not reachable" host))
-          :else                                                                 (response-invalid :db     "Unable to connect to database."))
-        (catch Throwable e
-          (response-invalid :dbname (.getMessage e)))))))
 
-;; TODO - Just make `:ssl` a `feature`
-(defn- supports-ssl?
-  "Does the given `engine` have an `:ssl` setting?"
-  [engine]
-  {:pre [(driver/is-engine? engine)]}
-  (let [driver-props (set (for [field (driver/details-fields (driver/engine->driver engine))]
-                            (:name field)))]
-    (contains? driver-props "ssl")))
+;;; ------------------------------------------------------------ GET /api/database ------------------------------------------------------------
 
 
 (defn- add-tables [dbs]
@@ -72,89 +47,15 @@
       []))
 
 
-(defendpoint POST "/"
-  "Add a new `Database`."
-  [:as {{:keys [name engine details is_full_sync]} :body}]
-  {name    [Required NonEmptyString]
-   engine  [Required DBEngine]
-   details [Required Dict]}
-  (check-superuser)
-  ;; this function tries connecting over ssl and non-ssl to establish a connection
-  ;; if it succeeds it returns the `details` that worked, otherwise it returns an error
-  (let [try-connection   (fn [engine details]
-                           (let [error (test-database-connection engine details)]
-                             (if (and error
-                                      (true? (:ssl details)))
-                               (recur engine (assoc details :ssl false))
-                               (or error details))))
-        details          (if (supports-ssl? engine)
-                           (assoc details :ssl true)
-                           details)
-        details-or-error (try-connection engine details)
-        is_full_sync     (if (nil? is_full_sync)
-                           true
-                           (boolean is_full_sync))]
-    (if-not (false? (:valid details-or-error))
-      ;; no error, proceed with creation
-      (let-500 [new-db (db/insert! Database, :name name, :engine engine, :details details-or-error, :is_full_sync is_full_sync)]
-        (events/publish-event :database-create new-db)
-        new-db)
-      ;; failed to connect, return error
-      {:status 400
-       :body   details-or-error})))
-
-
-(defendpoint POST "/sample_dataset"
-  "Add the sample dataset as a new `Database`."
-  []
-  (check-superuser)
-  (sample-data/add-sample-dataset!)
-  (Database :is_sample true))
+;;; ------------------------------------------------------------ GET /api/database/:id ------------------------------------------------------------
 
 (defendpoint GET "/:id"
   "Get `Database` with ID."
   [id]
   (check-404 (Database id)))
 
-(defendpoint PUT "/:id"
-  "Update a `Database`."
-  [id :as {{:keys [name engine details is_full_sync description caveats points_of_interest]} :body}]
-  {name    [Required NonEmptyString]
-   engine  [Required DBEngine]
-   details [Required Dict]}
-  (check-superuser)
-  (let-404 [database (Database id)]
-    (let [details      (if-not (= protected-password (:password details))
-                         details
-                         (assoc details :password (get-in database [:details :password])))
-          conn-error   (test-database-connection engine details)
-          is_full_sync (when-not (nil? is_full_sync)
-                         (boolean is_full_sync))]
-      (if-not conn-error
-        ;; no error, proceed with update
-        (do
-          ;; TODO: is there really a reason to let someone change the engine on an existing database?
-          ;;       that seems like the kind of thing that will almost never work in any practical way
-          (check-500 (db/update-non-nil-keys! Database id
-                       :name               name
-                       :engine             engine
-                       :details            details
-                       :is_full_sync       is_full_sync
-                       :description        description
-                       :caveats            caveats
-                       :points_of_interest points_of_interest)) ; TODO - this means one cannot unset the description. Does that matter?
-          (events/publish-event :database-update (Database id)))
-        ;; failed to connect, return error
-        {:status 400
-         :body   conn-error}))))
 
-(defendpoint DELETE "/:id"
-  "Delete a `Database`."
-  [id]
-  (let-404 [db (Database id)]
-    (write-check db)
-    (u/prog1 (db/cascade-delete! Database :id id)
-      (events/publish-event :database-delete db))))
+;;; ------------------------------------------------------------ GET /api/database/:id/metadata ------------------------------------------------------------
 
 (defendpoint GET "/:id/metadata"
   "Get metadata about a `Database`, including all of its `Tables` and `Fields`.
@@ -164,6 +65,8 @@
          read-check
          (hydrate [:tables [:fields :target :values] :segments :metrics])))
 
+
+;;; ------------------------------------------------------------ GET /api/database/:id/autocomplete_suggestions ------------------------------------------------------------
 
 (defn- autocomplete-tables [db-id prefix]
   (db/select [Table :id :db_id :schema :name]
@@ -210,11 +113,16 @@
       (log/warn "Error with autocomplete: " (.getMessage t)))))
 
 
+;;; ------------------------------------------------------------ GET /api/database/:id/tables ------------------------------------------------------------
+
 (defendpoint GET "/:id/tables"
   "Get a list of all `Tables` in `Database`."
   [id]
   (read-check Database id)
   (filter models/can-read? (db/select Table, :db_id id, :active true, {:order-by [:%lower.name]})))
+
+
+;;; ------------------------------------------------------------ GET /api/database/:id/fields ------------------------------------------------------------
 
 (defendpoint GET "/:id/fields"
   "Get a list of all `Fields` in `Database`."
@@ -229,12 +137,133 @@
      :table_name (:display_name table)
      :schema     (:schema table)}))
 
+;;; ------------------------------------------------------------ GET /api/database/:id/idfields ------------------------------------------------------------
+
 (defendpoint GET "/:id/idfields"
   "Get a list of all primary key `Fields` for `Database`."
   [id]
   (read-check Database id)
   (sort-by (comp s/lower-case :name :table) (filter models/can-read? (-> (database/pk-fields {:id id})
                                                                          (hydrate :table)))))
+
+
+;;; ------------------------------------------------------------ POST /api/database ------------------------------------------------------------
+
+(defn test-database-connection
+  "Try out the connection details for a database and useful error message if connection fails, returns `nil` if connection succeeds."
+  [engine {:keys [host port] :as details}]
+  (when-not config/is-test?
+    (let [engine           (keyword engine)
+          details          (assoc details :engine engine)
+          response-invalid (fn [field m] {:valid false
+                                          field m        ; work with the new {:field error-message} format
+                                          :message m})]  ; but be backwards-compatible with the UI as it exists right now
+      (try
+        (cond
+          (driver/can-connect-with-details? engine details :rethrow-exceptions) nil
+          (and host port (u/host-port-up? host port))                           (response-invalid :dbname (format "Connection to '%s:%d' successful, but could not connect to DB." host port))
+          (and host (u/host-up? host))                                          (response-invalid :port   (format "Connection to '%s' successful, but port %d is invalid." port))
+          host                                                                  (response-invalid :host   (format "'%s' is not reachable" host))
+          :else                                                                 (response-invalid :db     "Unable to connect to database."))
+        (catch Throwable e
+          (response-invalid :dbname (.getMessage e)))))))
+
+;; TODO - Just make `:ssl` a `feature`
+(defn- supports-ssl?
+  "Does the given `engine` have an `:ssl` setting?"
+  [engine]
+  {:pre [(driver/is-engine? engine)]}
+  (let [driver-props (set (for [field (driver/details-fields (driver/engine->driver engine))]
+                            (:name field)))]
+    (contains? driver-props "ssl")))
+
+(defendpoint POST "/"
+  "Add a new `Database`."
+  [:as {{:keys [name engine details is_full_sync]} :body}]
+  {name    [Required NonEmptyString]
+   engine  [Required DBEngine]
+   details [Required Dict]}
+  (check-superuser)
+  ;; this function tries connecting over ssl and non-ssl to establish a connection
+  ;; if it succeeds it returns the `details` that worked, otherwise it returns an error
+  (let [try-connection   (fn [engine details]
+                           (let [error (test-database-connection engine details)]
+                             (if (and error
+                                      (true? (:ssl details)))
+                               (recur engine (assoc details :ssl false))
+                               (or error details))))
+        details          (if (supports-ssl? engine)
+                           (assoc details :ssl true)
+                           details)
+        details-or-error (try-connection engine details)
+        is_full_sync     (if (nil? is_full_sync)
+                           true
+                           (boolean is_full_sync))]
+    (if-not (false? (:valid details-or-error))
+      ;; no error, proceed with creation
+      (let-500 [new-db (db/insert! Database, :name name, :engine engine, :details details-or-error, :is_full_sync is_full_sync)]
+        (events/publish-event :database-create new-db)
+        new-db)
+      ;; failed to connect, return error
+      {:status 400
+       :body   details-or-error})))
+
+
+;;; ------------------------------------------------------------ POST /api/database/sample_dataset ------------------------------------------------------------
+
+(defendpoint POST "/sample_dataset"
+  "Add the sample dataset as a new `Database`."
+  []
+  (check-superuser)
+  (sample-data/add-sample-dataset!)
+  (Database :is_sample true))
+
+;;; ------------------------------------------------------------ PUT /api/database/:id ------------------------------------------------------------
+
+(defendpoint PUT "/:id"
+  "Update a `Database`."
+  [id :as {{:keys [name engine details is_full_sync description caveats points_of_interest]} :body}]
+  {name    [Required NonEmptyString]
+   engine  [Required DBEngine]
+   details [Required Dict]}
+  (check-superuser)
+  (let-404 [database (Database id)]
+    (let [details      (if-not (= protected-password (:password details))
+                         details
+                         (assoc details :password (get-in database [:details :password])))
+          conn-error   (test-database-connection engine details)
+          is_full_sync (when-not (nil? is_full_sync)
+                         (boolean is_full_sync))]
+      (if-not conn-error
+        ;; no error, proceed with update
+        (do
+          ;; TODO: is there really a reason to let someone change the engine on an existing database?
+          ;;       that seems like the kind of thing that will almost never work in any practical way
+          (check-500 (db/update-non-nil-keys! Database id
+                       :name               name
+                       :engine             engine
+                       :details            details
+                       :is_full_sync       is_full_sync
+                       :description        description
+                       :caveats            caveats
+                       :points_of_interest points_of_interest)) ; TODO - this means one cannot unset the description. Does that matter?
+          (events/publish-event :database-update (Database id)))
+        ;; failed to connect, return error
+        {:status 400
+         :body   conn-error}))))
+
+;;; ------------------------------------------------------------ DELETE /api/database/:id ------------------------------------------------------------
+
+(defendpoint DELETE "/:id"
+  "Delete a `Database`."
+  [id]
+  (let-404 [db (Database id)]
+    (write-check db)
+    (u/prog1 (db/cascade-delete! Database :id id)
+      (events/publish-event :database-delete db))))
+
+
+;;; ------------------------------------------------------------ POST /api/database/:id/sync ------------------------------------------------------------
 
 ;; TODO - Shouldn't we just check for superuser status instead of write checking?
 (defendpoint POST "/:id/sync"
